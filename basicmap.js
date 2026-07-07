1da6e4f04c27e3310c065ab288eecc72ebe838f6
pantherkills44 <fastcheetah30@gmail.com>
2026-07-07 14:16:34 -0400

    Add CPCC marker to the map

--- basicmap.js
+++ basicmap.js
@@ -1,7 +1,9 @@
 // The latitude of Charlotte, NC, USA is 35.227085, and the longitude is -80.843124
 var charlotte = [35.227085, -80.843124]
+var cpcc = [35.21, -80.83];
 var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var cpccmarker = L.marker(cpcc).addTo(map);
