423bd950788c255f5bb6a95b348c589d0c0a75a1
pantherkills44 <fastcheetah30@gmail.com>
2026-07-07 14:10:44 -0400

    Change map view to center on Charlotte, NC
    Updated map initialization to center on Charlotte, NC.

--- basicmap.js
+++ basicmap.js
@@ -1,4 +1,6 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// The latitude of Charlotte, NC, USA is 35.227085, and the longitude is -80.843124
+var charlotte = [35.227085, -80.843124]
+var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
