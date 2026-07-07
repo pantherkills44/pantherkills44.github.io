472a056529c9e1932d158fc66f9dc481ff379b02
pantherkills44 <fastcheetah30@gmail.com>
2026-07-07 14:20:12 -0400

    Update CPCC coordinates for accuracy

--- basicmap.js
+++ basicmap.js
@@ -1,6 +1,6 @@
 // The latitude of Charlotte, NC, USA is 35.227085, and the longitude is -80.843124
 var charlotte = [35.227085, -80.843124]
-var cpcc = [35.21, -80.83];
+var cpcc = [35.2176665, -80.831473];
 var map = L.map('map').setView(charlotte, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
