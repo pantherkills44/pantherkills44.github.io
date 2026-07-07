9512ba77bd2b3a60fe48546161d37aab07587278
pantherkills44 <fastcheetah30@gmail.com>
2026-07-07 14:52:46 -0400

    Update CPCC marker popup content
    Removed width and height attributes from iframe in popup.

--- basicmap.js
+++ basicmap.js
@@ -7,5 +7,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var cpccmarker = L.marker(cpcc,{title:"CPCC Central Campus"}).bindPopup(
-    "<h4> CPCC Central Campus </h4><br><a href = 'https://www.cpcc.edu'> Visit cpcc.edu</a><br><iframe src = 'https://www.cpcc.edu' width='30%' height='100px'></iframe>"
+    "<h4> CPCC Central Campus </h4><br><a href = 'https://www.cpcc.edu'> Visit cpcc.edu</a><br><iframe src = 'https://www.cpcc.edu'></iframe>"
 ).addTo(map);
