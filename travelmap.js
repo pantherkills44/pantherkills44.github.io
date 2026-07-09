2e91a5c1228c95aa8cf43a113c8ae9a0ce1035fc
pantherkills44 <fastcheetah30@gmail.com>
2026-07-09 14:09:46 -0400

    Add markers for additional locations on the map

--- travelmap.js
+++ travelmap.js
@@ -1,8 +1,11 @@
 // 36.2048° N latitude and 138.2529° E longitude
 var coordinates = [36.2048, 138.2529]
 var map = L.map('map').setView(coordinates, 13);
+var place1 = [35.6594, 139.7006]
+var place2 = [35.6762, 139.6503]
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker(coordinates).bindPopup("<p>Hello, from Japan!!!</p>").addTo(map);
+var marker1 = L.circle(place1,{color: "black", fillColor: "#3b4040", radius: 80}).addTo(map);
