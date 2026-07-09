bff77eb0af280d4c68cb72c8992195df3515811a
pantherkills44 <fastcheetah30@gmail.com>
2026-07-09 13:49:28 -0400

    Fix map view coordinates syntax in travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -1,6 +1,6 @@
 // 36.2048° N latitude and 138.2529° E longitude
 var coordinates = [36.2048, 138.2529]
-var map = L.map('map').setView([coordinates, -0.09], 13);
+var map = L.map('map').setView(coordinates, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
